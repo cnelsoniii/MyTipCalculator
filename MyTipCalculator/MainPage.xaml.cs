36113b90d40beef467bfc245bc14faef9d17eb3c
Cornelius Nelson <corneliusnelson@Corneliuss-MacBook-Air.local>
2020-05-16 14:29:46 -0700

    First commit

---
+++ MyTipCalculator/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MyTipCalculator
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var billValue = int.Parse(bill.Text);
+            var tipPercentage = 200;
+            var splitValue = int.Parse(split.Text);
+
+            totalPerPerson.Text = CalculateTotalIncludingTip(billValue, tipPercentage).ToString();
+            bill.Text = billInput.ToString();
+            tip.Text = CalculateTip(billValue, tipPercentage).ToString();
+        }
+
+        int CalculateTotalIncludingTip(int billTotal, int tipPercentage)
+        {
+            var trueTipPercentage = tipPercentage / 100;
+            return billTotal * (1 + trueTipPercentage);
+        }
+
+        double CalculateTip(int billTotal, int tipPercentage)
+        {
+            var trueTipPercentage = tipPercentage / 100;
+            return billTotal * trueTipPercentage;
+        }
+
+    }
+}
